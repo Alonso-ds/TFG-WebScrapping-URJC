8c00f2c5e046522a1f134d64cec8ec2b662bb59f
Alonso-DS <alonsogalerna14@gmail.com>
2026-06-10 14:02:23 +0200

    Busqueda en el crawler con dos letras por maximo de 200 profesores

--- Services/Crawler.cs
+++ Services/Crawler.cs
@@ -18,37 +18,45 @@ public class Crawler
     public async Task RastreoAsync()
     {
         //await _context.Database.EnsureCreatedAsync();
-
-        for (char letra = 'A'; letra <= 'Z'; letra++)
+        for (char letra1 = 'A'; letra1 <= 'Z'; letra1++)
         {
-            Console.WriteLine($"Letra: {letra}");
-            List<DocenteDTO> dtos = await _scrapper.ScrapProfesor(letra.ToString());
-            foreach (var dto in dtos)
+            for (char letra2 = 'A'; letra2 <= 'Z'; letra2++)
             {
-                if (!string.IsNullOrWhiteSpace(dto.Nombre))
+                string busqueda = $"{letra1}{letra2}";
+                List<DocenteDTO> dtos = await _scrapper.ScrapProfesor(busqueda);
+                if(dtos == null ||dtos.Count == 0)
                 {
-                    var docenteExistente = await _context.Docentes.FirstOrDefaultAsync(d => d.UrlPerfil == dto.UrlPerfil);
-
-                    if (docenteExistente != null)
-                    {
-                        docenteExistente.Nombre = dto.Nombre;
-                        await _scrapper.ScrapDetallesProfesor(docenteExistente);
-                        Console.WriteLine($"Profesor actualizado: {docenteExistente.Nombre}");
-                    }
-                    else
-                    {
-                        var nuevoDocente = new Docente { Nombre = dto.Nombre, UrlPerfil = dto.UrlPerfil };
-                        await _scrapper.ScrapDetallesProfesor(nuevoDocente);
-                        _context.Add(nuevoDocente);
-                        Console.WriteLine($"Profesor nuevo: {nuevoDocente.Nombre}");
-                    }
+                    Console.WriteLine($"Busqueda {busqueda} sin resultados");
+                    continue;
                 }
-                await _context.SaveChangesAsync();
+                
+                foreach (var dto in dtos)
+                {
+                    if (!string.IsNullOrWhiteSpace(dto.Nombre))
+                    {
+                        var docenteExistente = await _context.Docentes.FirstOrDefaultAsync(d => d.UrlPerfil == dto.UrlPerfil);
+
+                        if (docenteExistente != null)
+                        {
+                            docenteExistente.Nombre = dto.Nombre;
+                            await _scrapper.ScrapDetallesProfesor(docenteExistente);
+                            Console.WriteLine($"Profesor actualizado: {docenteExistente.Nombre}");
+                        }
+                        else
+                        {
+                            var nuevoDocente = new Docente { Nombre = dto.Nombre, UrlPerfil = dto.UrlPerfil };
+                            await _scrapper.ScrapDetallesProfesor(nuevoDocente);
+                            _context.Add(nuevoDocente);
+                            Console.WriteLine($"Profesor nuevo: {nuevoDocente.Nombre}");
+                        }
+                    }
+                    await _context.SaveChangesAsync();
+                    await Task.Delay(1000);
+                }
+                //int filasInsertadas = await _context.SaveChangesAsync();
+                //Console.WriteLine($"{filasInsertadas} profesores con la letra {letra}");
                 await Task.Delay(2000);
             }
-            //int filasInsertadas = await _context.SaveChangesAsync();
-            //Console.WriteLine($"{filasInsertadas} profesores con la letra {letra}");
-            await Task.Delay(2000);
         }
     }
 }
